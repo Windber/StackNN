0ee361f0d831efda75554e6dd89c2cc3c19d97a2
lenovo <lenovo@59.66.122.7>
2019-04-21 13:35:07 +0800

    tmp.

--- structs/base.py
+++ structs/base.py
@@ -4,37 +4,6 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from stacknn_utils.profile import timeprofile
-class PDAStruct(nn.Module, metaclass=ABCMeta):
-    def __init__(self, embedding_size):
-        super(PDAStruct, self).__init__()
-        self._embedding_size = embedding_size
-        
-        self.contents = Variable(torch.FloatTensor(0))
-        self.strengths = Variable(torch.FloatTensor(0))
-        #self._readcontent = None
-    def forward(self, u, d1, d2, v1, v2, r=None):
-        self.push(d1, d2, v1, v2)
-        readcontent = self.read(u)
-        self.pop(u)
-        return readcontent
-    @abstractmethod
-    def _init_Struct(self, batch_size):
-        raise NotImplemented("Missing implementation for _init_Struct")
-    @abstractmethod
-    def pop(self, strength):
-        raise NotImplementedError("Missing implementation for pop")
-
-    @abstractmethod
-    def push(self, strength1, strength2, value1, value2):
-        raise NotImplementedError("Missing implementation for push")
-
-    @abstractmethod
-    def read(self, strength):
-        raise NotImplementedError("Missing implementation for read")
-
-    @property
-    def read_strength(self):
-        return 1.
 
 class Struct(nn.Module, metaclass=ABCMeta):
     """
